c7d4847c297723e5685d225ecb4972e053392f2a
Bhavya Antiya <103890469+bantiya@users.noreply.github.com>
2023-10-11 21:20:58 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "bhavya@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountNotSpecified
+
+accountEmail = "hc@hc.com"
+accountPassword = "321"
+accountCity = "Manipur"
+ 
+
+//accountId = 321
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountNotSpecified])
+
+/*+ */
