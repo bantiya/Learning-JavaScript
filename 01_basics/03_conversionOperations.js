cf1959b8ad342bbb691099f962bafd7aee6cabdb
Bhavya Antiya <103890469+bantiya@users.noreply.github.com>
2023-10-12 13:58:59 +0000

    Conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,43 @@
-let score = "123"
+/**
+ * Coverting to Number
+ */
 
-console.log(typeof score)
+let score = "Hitesh"
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber) // <== This will give NaN (Not a number)
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1, false => 0
+
+/**
+ * Converting to Boolean
+ */
+
+let isLoggedIn = 1
+let boolIsLoggedIn = Boolean(1)
+
+//console.log(typeof isLoggedIn)
+//console.log(boolIsLoggedIn) // <== This will give NaN (Not a number)
+
+
+// 1 => true, 0 => false
+// "" => false, "NonEmptyString" => true
+
+
+
+/**
+ * Converting to String
+ */
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
