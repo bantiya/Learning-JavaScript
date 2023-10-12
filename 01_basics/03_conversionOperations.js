9fb968f501a5193e23a572791b461f297d6acb84
Bhavya Antiya <103890469+bantiya@users.noreply.github.com>
2023-10-12 14:23:51 +0000

    Conversion complete

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -39,5 +39,35 @@ let boolIsLoggedIn = Boolean(1)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ********************************************* Operations *******************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "Bhavya"
+let str2 = " Antiya"
+
+let str3 = str1 + str2 
+// console.log(str3)
+
+/**
+ * Complex situation (Do not write suc messy) code
+ */
+
+// console.log("1" + 2); // => 12
+// console.log(1 + "2"); // => 12
+// console.log("1" + 2 + 2); // => 122
+// console.log(1 + 2 + "2"); // => 32
+
+// console.log(+true) // => 1
+// console.log(-true) // => -1
+
+let gameCounter = 100
+++gameCounter
+
+console.log(gameCounter);
