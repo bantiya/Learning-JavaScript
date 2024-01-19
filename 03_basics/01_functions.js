53d53f9b1eef3cc2c7538224c7b75df4a30e24ac
bantiya <bhavyaantiya@gmail.com>
2024-01-19 15:02:49 -0500

    Function in JS

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,7 @@
+function addTwoNumber(number1, number2) {
+    if(typeof(number1) == Number && typeof(number2) == Number) {
+        return number1 + number2
+    }
+}
+
+// +++++++++++++++++++++++++++++++++++++ Basic Functiona nd object stuff ++++++++++++++++++++++++++++++++++++
