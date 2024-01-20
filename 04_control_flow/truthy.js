bd6edf906df6c4a0d0097ea0026db922cd4e349a
bantiya <bhavyaantiya@gmail.com>
2024-01-19 19:39:35 -0500

    truthy values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,31 @@
+const userEmail = "bhavya1303@gmail.com"
+
+if (userEmail) {
+    console.log("Got user Email");
+}
+else {
+    console.log("Got user Email");
+}
+
+
+// ++++++++++++++++++++++++++++++++++++++++++ Falsy Values ++++++++++++++++++++++++++++++
+/**
+ * false
+ * 0
+ * -0
+ * BigInt (0n)
+ * null
+ * undefined
+ * NaN
+ */
+
+
+// ++++++++++++++++++++++++++++++++++++++++++ Truthy Values ++++++++++++++++++++++++++++++
+/**
+ * "0"
+ * "false"
+ * " "
+ * []
+ * {}
+ * function(){}    // <== Empty funtion
+ */
