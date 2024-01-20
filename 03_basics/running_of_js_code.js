433f5781c3b3c8552e854b62165f2f148db6093f
bantiya <bhavyaantiya@gmail.com>
2024-01-19 19:18:18 -0500

    Code run in JavaScript

---
+++ 03_basics/running_of_js_code.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ *  
+ * 1. Global Execution Contest
+ * 2. Function Execution Contest.
+ * 
+ * So JavaScript code runs in 2 parts:
+ * 
+ * 1. => Memory Creation Phase (Memory is created for all the variables)
+ * 2. => Execution phase (all the code is eecuted)
+ *       2.1 => If there is a function then a new environment is made and all the step are repeatd again.
+ */
