e0c0b4fe8a8d1428e99ed0c6a2f9de216c483a3f
bantiya <bhavyaantiya@gmail.com>
2024-01-19 19:33:08 -0500

    Basic if and switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+/** 
+ * Basic if Statement
+ */
+
+
+condition = false
+if(condition) {
+    let abc = "abc"
+    console.log(`If Statement ${abc}`);
+}
+else {
+    console.log(`Else Statement ${abc}`);   // abc out of scope
+}
