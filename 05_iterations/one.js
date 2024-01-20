35c2a4c9391d8f9694eaa184a73a6c392a85c4cf
bantiya <bhavyaantiya@gmail.com>
2024-01-19 22:12:59 -0500

    For Loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,26 @@
+/**
+ * Basic for loop
+ */
+
+
+const array = [1,2,3,4,5,6,7,8,9,0]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+    
+}
+
+
+// ++++++++++++++++++++++++++++++++++++++ BREAK +++++++++++++++++++++++++++++++
+
+/**
+ * Break will skip the loop
+ */
+
+
+// ++++++++++++++++++++++++++++++++++++++ CONTINUE +++++++++++++++++++++++++++++++
+
+/**
+ * Continue will skip that particular iteration(step)
+ */
