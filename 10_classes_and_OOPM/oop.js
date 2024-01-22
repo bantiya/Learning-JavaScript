8d6b25f0af83e0f5290bcc306a3648d546ad18fb
bantiya <bhavyaantiya@gmail.com>
2024-01-21 20:37:15 -0500

    small updates

---
+++ 10_classes_and_OOPM/oop.js
@@ -0,0 +1,13 @@
+// ++++++++++++++++++++++++++++++++++++++ Basic Class and Object ++++++++++++++++++++++++++++++++++++++
+
+const user = {
+    username : "Bhavya",
+    loginCount : 5,
+    signedInt : true
+}
+
+// ++++++++++++++++++++++++++++++++++++++ this.object == curren.object ++++++++++++++++++++++++++++++++++++++
+
+
+// ++++++++++++++++++++++++++++++++++++++ COnstructor Function (new Keyword) ++++++++++++++++++++++++++++++++++++++
+const today = new Date.now()
